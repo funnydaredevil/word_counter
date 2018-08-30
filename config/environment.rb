e011e293134f26e5917f4d45f814d3396c1d6e8b
Oleg Okunevych <okunevichov@gmail.com>
2018-08-30 13:05:34 +0300

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/word_counter'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/word_counter_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/word_counter_development'
+    #    adapter :sql, 'mysql://localhost/word_counter_development'
+    #
+    adapter :sql, ENV.fetch('DATABASE_URL')
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/word_counter/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
+    end
+  end
+end
